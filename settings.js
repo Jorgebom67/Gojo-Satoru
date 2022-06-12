2e582e29776179e6cb90efeb06519d6bae336ee1
Jorgebom67 <107344141+Jorgebom67@users.noreply.github.com>
2022-06-12 05:40:09 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,20 +19,20 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['447451244590']
+global.premium = ['447451244590']
+global.ownernomer = '447451244590'
+global.ownername = '🐦El Patron - MD'
+global.botname = 'Goku - Md'
+global.footer = '©El Patron'
 global.ig = 'https://github.com/nexusNw'
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
 global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.author = '🐦Yt'
 global.sessionName = 'session'
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.🐦','','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
